1a84dd65b2d94e2d9ca97cd4af35e6b8a8359148
sacret2 <mrbartosiak@gmail.com>
2019-11-30 08:01:43 +0100

    Project creation

---
+++ src/main/java/com/mkcomp/CarRentalApp/CarRentalAppApplication.java
@@ -0,0 +1,13 @@
+package com.mkcomp.CarRentalApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRentalAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mkcomp/CarRentalApp/CarRentalAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mkcomp.CarRentalApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
