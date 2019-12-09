b236adbb6723cae95e106cf6d881860a9bd2e33d
Kamil Nowosad <kamilnowosad1@gmail.com>
2019-12-09 10:31:38 +0100

    login, register views

---
+++ src/main/java/com/mkcomp/CarRentalApp/api/request/LoginRequest.java
@@ -0,0 +1,22 @@
+package com.mkcomp.CarRentalApp.api.request;
+
+public class LoginRequest {
+    public String userName;
+    public String password;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
--- src/main/java/com/mkcomp/CarRentalApp/controller/HomeController.java
+++ src/main/java/com/mkcomp/CarRentalApp/controller/HomeController.java
@@ -1,14 +1,36 @@
 package com.mkcomp.CarRentalApp.controller;
 
+import com.mkcomp.CarRentalApp.api.request.AddCustomerRequest;
+import com.mkcomp.CarRentalApp.api.request.LoginRequest;
+import com.mkcomp.CarRentalApp.service.CustomerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
     @RequestMapping("/")
-    public String viewHomePage() {
+    public String viewHomePage(Model model) {
+        model.addAttribute("theDate", new java.util.Date());
+
         return "index";
     }
+
+    @RequestMapping("/login")
+    public String viewLoginForm(Model model) {
+        LoginRequest loginRequest = new LoginRequest();
+        model.addAttribute("loginRequest", loginRequest);
+        return "login";
+    }
+
+    @RequestMapping("/register")
+    public String viewRegisterForm(Model model) {
+        AddCustomerRequest addCustomerRequest = new AddCustomerRequest();
+        model.addAttribute("addCustomerRequest", addCustomerRequest);
+        return "register";
+    }
+
+
 
 }
